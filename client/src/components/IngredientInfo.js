a0339a0b3ad8993bf9c5877c51955c16ddb0b1e1
Alex Davidoff <ad35@pdx.edu>
2020-08-01 14:12:01 -0700

    small fix

--- client/src/components/IngredientInfo.js
+++ client/src/components/IngredientInfo.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types'
 import ReactHtmlParser from 'react-html-parser'
 import DataComponent from './DataComponent.js'
 import App from './App.js'
-import Card from './Card.js'
 
 /*const IngredientViz = ({ data }) => (
     <div className="ingredient-widget">
