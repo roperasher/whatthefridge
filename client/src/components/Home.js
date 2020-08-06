74d18f485f75a4a1421bec3a74f263adf4333e08
Jordan Co <cojor@pdx.edu>
2020-08-06 14:52:45 -0700

    initial home page and add background image

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,31 @@
 import React from "react";
-import { withRouter } from 'react-router-dom';
+import { withRouter } from "react-router-dom";
+import { Jumbotron, Button, Modal } from "react-bootstrap";
 
 function Home() {
   return (
-    <div className="mt-d d-flex justify-content-center">
-      <h3>This is the home page!</h3>
+    <div className="my-5 d-flex justify-content-center">
+    <div className="row">
+      <div className="col-sm-2"></div> 
+      <div className="col-sm-8">
+      <Jumbotron className="border border-secondary">
+        <h1 className="mb-3">Welcome to What the Fridge!!</h1>
+          <p className="text-left">
+            Have you ever opened your fridge in a hungry fervor and had no idea
+            what to do with the strange bits and bobs in the back corner of your
+            fridge? Well you are in luck my friend, What the Fridge aims to help
+            you find the right recipes for the ingredients you have.
+          </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </Jumbotron>
+
+      </div> 
+      <div className="col-sm-2"></div> 
+    </div>
     </div>
   );
 }
-
 
 export default withRouter(Home);
