837e4a311d020d17f8bdc0af87c1644ef47e17f8
Jordan Co <cojor@pdx.edu>
2020-08-07 00:22:36 -0700

    Improved search ingredient. Works with enter button and should reduce API calls

--- client/src/components/Fridge/SearchIngr.js
+++ client/src/components/Fridge/SearchIngr.js
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import { FormControl, Form, Button } from "react-bootstrap";
 import url from "url";
 
-
-function SearchIngr({ addIngr=f=>f }) {
+function SearchIngr({ addIngr = (f) => f }) {
   const API_KEY = process.env.REACT_APP_API_KEY;
+  const [text, setText] = useState("");
   const spoonURL = new URL(
     `https://api.spoonacular.com/food/ingredients/autocomplete?`
   );
@@ -12,8 +12,8 @@ function SearchIngr({ addIngr=f=>f }) {
   let newIngr = {
     name: "",
     image: "",
-    loaded: false
-  }
+    loaded: false,
+  };
 
   let params = new URLSearchParams([
     ["apiKey", `${API_KEY}`],
@@ -21,12 +21,12 @@ function SearchIngr({ addIngr=f=>f }) {
     ["query", ""],
   ]);
 
-  const handleChange = (e) => {
-    const target = e.target;
-    const val = target.value;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const val = text;
+    setText('');
     params.set("query", val);
     spoonURL.search = params.toString();
-
     if (val.length >= 3) {
       fetch(spoonURL.href)
         .then((resp) => resp.json())
@@ -36,21 +36,33 @@ function SearchIngr({ addIngr=f=>f }) {
             image: data[0].image,
             loaded: true
           }
+          console.log(`adding Ingredient ${newIngr.name}`);
+          addIngr(newIngr);
         })
         .catch((error) => console.log(error));
     }
+  }
+
+  const handleChange = (e) => {
+    const target = e.target;
+    const val = target.value;
+
+    setText(val);
   };
 
   return (
-    <Form inline>
+    <Form inline onSubmit={handleSubmit}>
       <FormControl
         name="searchtext"
         type="text"
+        value={text}
         onChange={handleChange}
         placeholder="Search Ingredients "
         className="mr-sm-2"
       />
-      <Button variant="outline-success" onClick={() => (newIngr.loaded) ? addIngr(newIngr) : ""}>Add Ingredient</Button>
+      <Button variant="outline-success" type="submit">
+        Add Ingredient
+      </Button>
     </Form>
   );
 }
