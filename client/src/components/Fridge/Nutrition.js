8575bf084970c371b7245ca52ce8fc31cf9b8ee2
Jordan Co <cojor@pdx.edu>
2020-08-06 21:05:51 -0700

    flip cart test for Nutrition

--- client/src/components/Fridge/Nutrition.js
+++ client/src/components/Fridge/Nutrition.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React from "react";
+import "../../stylesheets/Nutrition.css";
 
 const Nutrition = () => {
-        return(
-            <div className="mt-d d-flex justify-content-center">
-                <h3>Nutrition component placeholder</h3>
-            </div>
-        )
-    }
+  return (
+    <div className="mt-d d-flex justify-content-center">
+      <h3>Nutrition component placeholder</h3>
+      <FlipCard />
+    </div>
+  );
+};
+
+class FlipCard extends React.Component {
+
+  handleClick() {
+    this.refs.Card.classList.toggle("backCard");
+    this.refs.Card.classList.toggle("frontCard");
+    this.refs.frontCard.classList.toggle("deactive");
+    this.refs.frontCard.classList.toggle("active");
+    this.refs.backCard.classList.toggle("deactive");
+    this.refs.backCard.classList.toggle("active");
+  }
+
+    render(){
+    return (
+      <div>
+        <div 
+          ref="Card" 
+          className="frontCard"
+          onClick={this.handleClick.bind(this)}>
+          <div 
+            ref="frontCard" 
+            className="active">Front Card</div>
+          <div 
+            ref="backCard" 
+            className="back deactive">Back Card</div>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Nutrition;
