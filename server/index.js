e19a5be3b7abae5defe4ebf4469b3ceb5964f57d
Jordan Co <cojor@pdx.edu>
2020-08-08 17:43:58 -0700

    updated index.js for heroku deployment

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const path = require("path");
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
+const cors = require('cors')
+const ingredient = require('./routes/ingredient.js');
+const recipe = require('./routes/recipe.js');
+const nutrition = require('./routes/nutrition.js');
 
 const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
@@ -26,12 +30,26 @@ if (!isDev && cluster.isMaster) {
 
 } else {
   const app = express();
-
+  app.use(cors());
   // Priority serve any static files.
   app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-  // Answer API requests.
-  app.use("/recipes", require("./routes/recipe"));
+  // // Answer API requests.
+  // app.use("/recipes", require("./routes/recipe"));
+
+  //endpoints for ingredient, recipe, and nutrition data
+  app.get("/data/ingredient/searchGroceryProducts", ingredient.requestProductData);
+  app.get("/data/ingredient/getProductInformation", ingredient.requestProductIngredients);
+  app.get("/data/recipe/searchRecipe", recipe.getRecipeData);
+  app.get("/data/recipe/searchRecipeID", recipe.getRecipeDataID)
+  app.get("/data/recipe/searchRecipesByIngredients", recipe.getRecipeDataByIngredients);
+  app.get("/data/recipe/visualizeRecipeByIngredientsID", recipe.getRecipeIngredientCssID);
+  app.get("/data/recipe/visualizeRecipeByIngredients", recipe.getRecipeIngredientCSS);
+  app.get("/data/recipe/visualizeRecipePriceBreakdownByID", recipe.getRecipePrice);
+  app.get("/data/recipe/getRecipeIngredientsByID", recipe.requestRecipeIngredients)
+  app.get("/data/nutrition/visualizeRecipeNutrition", nutrition.getRecipeNutrition);
+  app.get("/data/nutrition/getNutritionInformation", nutrition.getRecipeNutritionID);
+
 
   // All remaining requests return the React app, so it can handle routing.
   app.get('*', function(request, response) {
