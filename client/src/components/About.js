7bfd4df42e7cd13215111dd22e2d31689e02e100
Asher Roper <roper@pdx.edu>
2020-08-09 13:51:50 -0700

    uncommented jokes. Updated the About page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -10,10 +10,26 @@ export class About extends Component {
             <div className="aboutBox">
                 <h3>About What the Fridge</h3>
                 <p>
-                    Insert Text here about:
-                        - fullstack project
-                        - why make this app
-                        - API and technology used 
+                    Welcome to WhatTheFridge! In this application, you can build your own virtual fridge and have
+                    recipes recommended to you based on the ingredients in it. By going to "My Fridge" and searching
+                    for ingredients in the top right corner of the application, you can add ingredients to your frige.
+                    Once your virtual frige represents your real one, you can go to "Search Recipes" to view potential
+                    recipes to make. From there, you can add the recipes which you would like to make by adding it to
+                    your fridge. You can also click on ingredients and recipes to get nutrition analytics, cooking
+                    instructions, and more.
+
+                    Developers:
+                    Alex Davidoff
+                    Jordan Co
+                    Asher Roper
+
+                    Technologies Used:
+                    Node.js
+                    Express
+                    React
+                    React Chart 
+                    React Notify
+                    Spoonacular (API)
                 </p>
             </div>
           </div>
