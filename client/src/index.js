861f6ea373bf0fdfe57053dddbab7952c32f0e95
adavidoff425 <48543352+adavidoff425@users.noreply.github.com>
2020-08-10 14:40:24 -0700

    Revert "Revert "Revert 24 dev""

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import React from 'react'
-import { render } from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
-import App from './components/App'
-import * as serviceWorker from './serviceWorker'
-
-window.React = React
-
-require('dotenv').config();
-render(
-  <App />,
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
